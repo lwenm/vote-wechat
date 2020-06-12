31110a8f9f3252594ef2bb60162f70ea3ae8d9d3
yue230694 <cy868768@163.com>
2020-06-12 16:03:02 +0800

    修改成功页颜色

--- miniprogram/tech_vote/pages/publishlist/successA/success.js
+++ miniprogram/tech_vote/pages/publishlist/successA/success.js
@@ -22,10 +22,6 @@ Page({
     wx.setNavigationBarTitle({
       title: '投票结束'
     })
-    wx.setNavigationBarColor({
-      frontColor: '#000000',
-      backgroundColor: '#ffffff'
-    });
   },
 
   /**
